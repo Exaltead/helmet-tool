083786bdb485fcb28e5b592405a123d7194ded2f
exaltead <aku.pohjola@gmail.com>
2025-03-11 18:55:47 +0200

    feat: added simple top bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import TopBar from '@/components/TopBar.vue';
 </script>
 
 <template>
-  <!--div class="bg-light-gray w-full h-full">
-    <RouterView/>
-  </div-->
+  <TopBar />
   <RouterView />
 </template>
---
+++ frontend/src/components/TopBar.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import IconMenu from './icons/IconMenu.vue';
+
+</script>
+
+<template>
+  <div class="w-full h-10 bg-brand-primary p-2">
+    <IconMenu class="w-6 text-white"/>
+  </div>
+</template>
