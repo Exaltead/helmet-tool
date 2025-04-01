c84ad2b4a3ed89191bcd57f9dbc583d9e9737a7f
exaltead <aku.pohjola@gmail.com>
2025-04-01 21:05:26 +0300

    feat: added logout functionality

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 import { computed, ref } from 'vue';
 import IconMenu from './icons/IconMenu.vue';
+import { useRouter } from 'vue-router';
+import { clearTokens } from '@/modules/auth-store';
 
 const links = ref([
   "Kirjat", "Pelit", "Haasteet"
@@ -16,11 +18,22 @@ function toggleNavOpen() {
   navOpen.value = !navOpen.value
 }
 
+const router = useRouter()
+
+const showNavLinks = computed(() => {
+  return router.currentRoute.value.name !== "login"
+})
+
+function doLogout(){
+  clearTokens()
+  navOpen.value = false
+  router.push({ name: "login" })
+}
 
 </script>
 
 <template>
-  <div class=" md:flex bg-brand-primary p-2 md:flex justify-between">
+  <div v-if="showNavLinks" class="md:flex bg-brand-primary p-2 md:flex justify-between">
     <div class=" flex flex-row gap-6">
       <button @click="toggleNavOpen" class="cursor-pointer md:hidden">
         <IconMenu class="w-6 text-white" />
@@ -30,10 +43,21 @@ function toggleNavOpen() {
 
 
     <div class="left-0 md:static absolute md:w-auto bg-brand-primary w-full" :class="navClassName">
-      <ul class="md:flex md:items-center md:gap-4 ">
-        <li class="text-white" v-for="link in links" :key="link"> {{ link }}</li>
-      </ul>
+      <div class="md:flex md:flex-row p-4 md:py-0">
+        <ul class="md:flex md:items-center gap-4 ">
+          <li class="text-white" v-for="link in links" :key="link"> {{ link }}</li>
+        </ul>
+
+
+        <div class="mt-10 md:mt-0 md:pl-4 text-white border w-fit p-2 rounded-lg">
+          <button :onClick="doLogout" class="cursor-pointer">Kirjaudu ulos</button>
+        </div>
+      </div>
+
     </div>
 
+  </div>
+  <div v-else class="bg-brand-primary p-2">
+    <h1 class="font-bold text-white">Tikeli-li</h1>
   </div>
 </template>
