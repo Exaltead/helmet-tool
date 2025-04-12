98e7e9a704e8954f0e49c8dc346e23995c98ffe2
exaltead <aku.pohjola@gmail.com>
2025-04-12 07:29:27 +0300

    feat: minor ui improvement

--- frontend/src/components/ChallengeManagement/ManageChallenges.vue
+++ frontend/src/components/ChallengeManagement/ManageChallenges.vue
@@ -16,14 +16,21 @@ function handleEdit(challenge: Challenge): void {
 <template>
   <div class="p-10">
     <BrandedButton text="Luo uusi haaste" :onClick="() => emit('create-challenge')" />
-    <ul class="flex flex-col gap-4">
-      <li v-for="challenge in challenges" :key="challenge.id">
-        <div class="flex flex-row gap-10 items-center ">
-          <span>{{ challenge.name }}</span>
-          <BrandedButton text="Muokkaa" :onClick="() => { handleEdit(challenge) }" />
-        </div>
+    <div class="flex items-center justify-center">
+      <ul class="flex flex-col gap-4">
+        <li v-for="challenge in challenges" :key="challenge.id">
+          <div class="flex items-center bg-white shadow-lg rounded px-4 py-2 flex flex-col gap-2 w-100">
+            <div>
+              <span class="font-bold text-xl">{{ challenge.name }}</span>
+            </div>
+            <div class="flex flex-row justify-end w-full">
+              <BrandedButton text="Muokkaa" :onClick="() => { handleEdit(challenge) }" />
+            </div>
 
-      </li>
-    </ul>
+          </div>
+
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
