a505ef9e699b00ddc2e58784d5f36d38b01d9dd3
exaltead <aku.pohjola@gmail.com>
2025-05-20 06:37:51 +0300

    feat: created a pill button for new items

--- frontend/src/components/basics/BrandedButton.vue
+++ frontend/src/components/basics/BrandedButton.vue
@@ -1,8 +1,15 @@
 <script lang="ts" setup>
 import IconBack from "@/components/icons/IconBack.vue"
 import IconDelete from "@/components/icons/IconDelete.vue"
+import IconPlus from "@/components/icons/IconPlus.vue"
 
-type IconName = "back" | "delete"
+type IconName = "back" | "delete" | "plus"
+
+type ButtonStyle = {
+  isPill?: boolean
+  backgroundColor?: "primary" | "warm-white"
+  bold?: boolean
+}
 
 defineProps<{
   onClick: () => void
@@ -10,29 +17,58 @@ defineProps<{
   isSubmitting?: boolean
   icon?: IconName
   disabled?: boolean
+  styling?: ButtonStyle
 }>()
 
-function createButtonStyle(disabled: boolean){
-  return disabled
-    ? "bg-brand-disabled text-text-disabled cursor-not-allowed py-1 px-2"
-    : "bg-brand-primary text-text-primary font-bold rounded cursor-pointer py-1 px-2"
+function createButtonStyle(disabled: boolean, style: ButtonStyle | undefined) {
+  let baseStyle = "py-1 pl-2 pr-3"
+
+  let backgroundColor = "bg-brand-primary"
+  if (style?.backgroundColor === "warm-white") {
+    backgroundColor = "bg-brand-warm-white"
+  }
+
+  baseStyle = baseStyle + " " + backgroundColor
+
+  if (style?.isPill) {
+    baseStyle = baseStyle + " rounded-full border border-brand-black"
+  } else {
+    baseStyle = baseStyle + " rounded border border-brand-black"
+  }
+
+  if (disabled) {
+    if (!style?.backgroundColor) {
+      baseStyle = baseStyle + " bg-brand-disabled"
+    }
+    baseStyle = baseStyle + " cursor-not-allowed text-text-disabled"
+  } else {
+    baseStyle = baseStyle + " cursor-pointer text-black"
+  }
+  if (style?.bold) {
+    baseStyle = baseStyle + " font-bold"
+  }
+
+  return baseStyle
 }
 
 </script>
 
 <template>
-  <button @click="onClick" :class="createButtonStyle(disabled ??false)" type="button" :disabled="disabled">
-    <div class="flex flex-row gap-2 items-center">
+  <button @click="onClick" :class="createButtonStyle(disabled ?? false, styling)" type="button" :disabled="disabled">
+    <div class="flex flex-row gap-2 items-center justify-start">
       <div v-if="isSubmitting"
         class="bg-brand-primary text-text-primary mr-3 size-5 animate-spin rounded-full border-4 border-white border-t-transparent">
       </div>
+      <div v-if="icon && icon === 'plus'">
+        <IconPlus class="text-brand-primary w-[22px] h-fit" />
+      </div>
       <div v-if="icon && icon === 'back'">
-        <IconBack class="text-text-primary w-4 h-fit" />
+        <IconBack class="text-text-primary w-[22px] h-fit" />
       </div>
       <div v-if="icon && icon === 'delete'">
-        <IconDelete class="text-text-primary w-4 h-fit" />
+        <IconDelete class="text-text-primary w-[22px] h-fit" />
       </div>
-      <span v-if="text" class="px-4">{{ text }}</span>
+      <span v-if="text" class="text-black text-nowrap text-center">{{ text }}</span>
     </div>
   </button>
 </template>
--- frontend/src/views/EntryListingView.vue
+++ frontend/src/views/EntryListingView.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
+import IconPlus from '@/components/icons/IconPlus.vue';
 import EntryListing from '../components/EntryListing/EntryListing.vue'
+import BrandedButton from '@/components/basics/BrandedButton.vue';
+
+function createNew() {
+  console.log("Creating new entry");
+}
+
 </script>
 
 <template>
   <main>
+    <div>
 
-    <EntryListing />
+    </div>
+    <div class="flex flex-col gap-1 w-full h-full">
+      <div class="flex flex-row justify-start items-center py-2 px-1.5">
+        <BrandedButton text="Lisää uusi" :onClick="createNew" icon="plus"
+          :styling="{ isPill: true, backgroundColor: 'warm-white', bold: true }" />
+      </div>
+
+
+
+      <EntryListing class="px-1" />
+    </div>
+
 
   </main>
 </template>
